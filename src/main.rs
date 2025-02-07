5ed83948d9d49563f412249aebf9cbb01b48380c
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-02-07 20:13:45 +0000

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-use bevy::prelude::*;
+use bevy::{
+    app::{App, Startup},
+    asset::Assets,
+    core_pipeline::core_2d::Camera2dBundle,
+    ecs::system::{Commands, ResMut},
+    prelude::default,
+    render::mesh::{shape::Circle, Mesh},
+    sprite::ColorMesh2dBundle,
+    DefaultPlugins,
+};
 
 fn main() {
-    App::new().add_systems(Update, hello_world).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
 }
 
-fn hello_world() {
-    println!("hello world!");
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(ColorMesh2dBundle {
+        mesh: meshes.add(Circle::new(50.).into()).into(),
+        ..default()
+    });
 }
